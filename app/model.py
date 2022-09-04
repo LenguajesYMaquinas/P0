fc94ad82a7de5960742aaa38cadb22ad46444dad
sfrancop <s.francop@uniandes.edu.co>
2022-09-04 13:26:40 -0500

    verify opened-closed program and var declarations

--- app/model.py
+++ app/model.py
@@ -15,16 +15,47 @@ def file_reader(route: str)->list:
 
     return file_lines
 
-def verify_opened_closed_instructions(program: list)->bool:
+def verify_opened_closed_program(program:list)->bool:
 
-    closed_instructions = {"PROG": False, "keys": {}}
+    output = False
+    states = {
+            "PROG": False,
+            "GORP": False, 
+        }
 
     for i in program:
         if "PROG" in i:
-            closed_instructions["PROG"] = True if closed_instructions["PROG"] == False else False
+            states["PROG"] = True if states["PROG"] == False else False
+        if "GORP" in i:
+            states["GORP"] = True if states["GORP"] == False else False
 
-    print(closed_instructions["PROG"])
+    if states["PROG"]==True and states["GORP"]==True:
+        output = True
+
+    return output
+
+def verifiy_var_declarations(program:list)->bool:
+
+    output = False
+    var_counter = 1
+    states = {}
+
+    for i in program:
+        if "var" in i:
+            states["var_"+str(var_counter)] = False
+            if len(i) > 1 and i[len(i)-1][len(i[len(i)-1])-1] == ";":
+                states["var_"+str(var_counter)] = True
+            var_counter += 1
+
+    if False not in list(states.values()):
+        output = True
+
+    return output
+
+            
 
 def verify_program(program)->bool:
-    if verify_opened_closed_instructions:
-        print(True)
+    if verify_opened_closed_program(program) and verifiy_var_declarations(program):
+        print("\nThe program is correct.\n")
+    else:
+        print("\nThe program is not correct.\n")
---
+++ tests.py
@@ -0,0 +1,2 @@
+i = ['var', 'n,', 'x,', 'y;']
+print(i[len(i)-1][len(i[len(i)-1])-1])
