98c780f45c03a9d35c2e4f9b002b8824edc9d822
Sergio Franco <88736607+sfrancop@users.noreply.github.com>
2022-09-05 00:14:23 -0500

    Update view.py

--- app/view.py
+++ app/view.py
@@ -1,10 +1,11 @@
 import model
 
-file_route = "./data/example_1.txt"
+file_route = "./data/"
 
 def execute_appplication():
 
-    route = input("Please, enter the route of the text file: ")
+    route = input("Please, enter the name of the text file: ")
+    file_route = file_route + route
     program = model.file_reader(file_route)
     model.verify_program(program)
 
