115f546d0cf927e409873664bd725159f81c8228
sfrancop <s.francop@uniandes.edu.co>
2022-09-06 17:18:16 -0500

    verify opened-closed procedures and verify open-closed procedure correction

--- app/model.py
+++ app/model.py
@@ -7,10 +7,31 @@ instructions = ["walk", "jump", "jumpTo", "veer", "look", "drop", "grab", "get",
 
 "---------------------Auxiliar functions---------------------"
 
-def number_of_arguments():
-    return True
+def confirmar_PC(parte, pos):
 
-"---------------------Main functions---------------------"
+    for o in range(pos, len(parte)):
+        Lugar = parte[o] 
+        LugarI = len(Lugar)
+        todoBien= " "
+        if LugarI == 1 or LugarI == 2:
+            comprobar= Lugar[-1]
+        else:
+            todoBien= False
+            break
+
+        if comprobar == "fi" or comprobar == "od" or comprobar == "per" or comprobar == "{":
+            todoBien= True
+        elif comprobar == "}":
+            todoBien= True
+            break
+        else:
+            sig= parte[o+1]
+            if sig == "}":
+                todoBien= True
+
+        return todoBien
+
+"---------------------File reader function---------------------"
 
 def file_reader(route: str)->list:
 
@@ -36,6 +57,33 @@ def file_reader(route: str)->list:
 
     return file_lines
 
+"---------------------Verifyer functions---------------------"
+
+def verify_opened_closed_procedure(program:list)->bool:
+    
+    """
+
+    Verify a procedure was closed and opened.
+
+    """
+
+    output = False
+    states = {
+            "PROC": 0,
+            "CORP": 0, 
+        }
+
+    for i in program:
+        if "PROC" in i:
+            states["PROC"] = states["PROC"] + 1
+        if "CORP" in i:
+            states["CORP"] = states["CORP"] + 1
+
+    if states["PROC"]==states["CORP"]:
+        output = True
+
+    return output
+
 def verify_opened_closed_program(program:list)->bool:
 
     """
@@ -46,18 +94,20 @@ def verify_opened_closed_program(program:list)->bool:
 
     output = False
     states = {
-            "PROG": False,
-            "GORP": False, 
+            "PROG": 0,
+            "GORP": 0, 
         }
 
     for i in program:
         if "PROG" in i:
-            states["PROG"] = True if states["PROG"] == False else False
+            states["PROG"] = states["PROG"] + 1
         if "GORP" in i:
-            states["GORP"] = True if states["GORP"] == False else False
+            states["GORP"] = states["GORP"] + 1
 
-    if states["PROG"]==True and states["GORP"]==True:
+    if states["PROG"]==states["GORP"] and states["PROG"]!=0 and states["GORP"]!=0:
         output = True
+
+    return output
 
     return output
 
@@ -101,99 +151,23 @@ def declare_variables(program: list):
                 name = name.replace(";", "")
                 declared_variables.append(name)
 
-def verify_declared_variables(program:list)->bool:
+def verify_arguemts(program:list)->bool:
 
     """
     
-        Verify that the variables inserted in the argument of a command were declared
+        Verify that the parameters inserted as arguments of a command are valid.
 
     """
-
-    states = {}
 
     for line in program:
         for string in program:
             if "walk" in string:
-        
+                return True
+
 "---------------------Main function---------------------"
 
-def confirmar_PC(parte, pos):
-
-    for o in range(pos, len(parte)):
-        Lugar = parte[o] 
-        LugarI = len(Lugar)
-        todoBien= " "
-        if LugarI == 1 or LugarI == 2:
-            comprobar= Lugar[-1]
-        else:
-            todoBien= False
-            break
-
-        if comprobar == "fi" or comprobar == "od" or comprobar == "per" or comprobar == "{":
-            todoBien= True
-        elif comprobar == "}":
-            todoBien= True
-            break
-        else:
-            sig= parte[o+1]
-            if sig == "}":
-                todoBien= True
-
-        return todoBien
-
-
-def verificar_secuancia(program):
-
-    conclusion= " "
-    posicion= 0
-    for i in program:
-        
-        cerrar_P= 0
-
-        if i == "{":
-            cerrar_P+= 1
-            a = confirmar_PC(program, posicion)
-
-            if a == False:
-                conclusion= False
-
-        posicion+= 1
-
-    return conclusion 
-
-def verificar_Proc(program):
-    conclusion= " "
-    if program[0] == "PROG":
-        program.pop(0)
-    else:
-        conclusion= False
-        return conclusion
-    
-    if program[-1] == "CORP":
-        program.pop(-1)
-    else:
-        conclusion= False
-        return conclusion
-
-    for i in program:
-        control= 0
-        if program[i] == "PROG":
-            control+= 1
-
-        if program[i] == "CORP":
-            control-= 1
-
-    if control != 0:
-        conclusion= False
-        return conclusion 
-    else:
-        conclusion= True
-
-    return conclusion
-
-
 def verify_program(program)->bool:
-    if verify_opened_closed_program(program) and verifiy_var_declarations(program) and verificar_secuancia(program):
+    if verify_opened_closed_program(program) and verifiy_var_declarations(program) and verify_opened_closed_procedure(program):
         print("\nThe program is correct.\n")
     else:
         print("\nThe program is not correct.\n")
--- app/view.py
+++ app/view.py
@@ -1,12 +1,12 @@
 import model
 
-file_route = "./data/"
-
 def execute_appplication():
 
+    file_route = "./data/"
     route = input("Please, enter the name of the text file: ")
     file_route = file_route + route
     program = model.file_reader(file_route)
     model.verify_program(program)
+    
 
 execute_appplication()
